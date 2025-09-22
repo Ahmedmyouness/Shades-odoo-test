807b07cd5c22d548583d286aff758c50575f17ac
Ahmedmyouness <97630305+Ahmedmyouness@users.noreply.github.com>
2025-09-23 00:35:07 +0300

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+echo "➡️ Running Odoo DB initialization..."
+odoo -i base \
+  -d odoo_db_8syn \
+  --db_host=$DB_HOST \
+  --db_port=$DB_PORT \
+  --db_user=$DB_USER \
+  --db_password=$DB_PASSWORD \
+  --without-demo=all \
+  --stop-after-init || true
+
+echo "➡️ Starting Odoo server..."
+exec odoo -c /etc/odoo/odoo.conf
