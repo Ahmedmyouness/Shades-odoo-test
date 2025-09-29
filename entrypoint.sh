4b276c09d9a939dc9834659a6210cf689307ba8a
Ahmedmyouness <97630305+Ahmedmyouness@users.noreply.github.com>
2025-09-29 21:30:44 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 set -e
 
-echo "➡ First-time DB initialization..."
-odoo -c /etc/odoo/odoo.conf -i base --without-demo=all
+echo "➡ Waiting for PostgreSQL..."
+until pg_isready -h $DB_HOST -p $DB_PORT -U $DB_USER >/dev/null 2>&1; do
+  sleep 2
+done
 
-echo "➡ Starting Odoo server..."
+echo "➡ PostgreSQL is up."
+
+echo "➡ Checking if Odoo database is initialized..."
+if ! PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -p $DB_PORT -U $DB_USER -d $DB_NAME -c '\dt' | grep -q ir_module_module; then
+    echo "➡ Database is empty, initializing Odoo..."
+    odoo -c /etc/odoo/odoo.conf -i base --without-demo=all --stop-after-init
+fi
+
+echo "➡ Starting Odoo..."
 exec odoo -c /etc/odoo/odoo.conf
