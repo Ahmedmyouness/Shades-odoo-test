8312705b3a81419774e25e8df942ab2b53a32e15
Ahmedmyouness <97630305+Ahmedmyouness@users.noreply.github.com>
2025-09-29 21:02:18 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,6 @@
 set -e
 
 echo "➡ Checking PostgreSQL connection..."
-echo "   Using host=$DB_HOST port=$DB_PORT user=$DB_USER db=$DB_NAME"
-
 until pg_isready -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER"; do
   echo "⏳ Waiting for PostgreSQL at $DB_HOST:$DB_PORT..."
   sleep 2
