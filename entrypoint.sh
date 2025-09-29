02e65b2e66013d1377b9cab2366c5c0cc954d9f0
Ahmedmyouness <97630305+Ahmedmyouness@users.noreply.github.com>
2025-09-29 21:18:41 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -e
 
-echo "➡ Starting Odoo server using odoo.conf..."
+echo "➡ First-time DB initialization..."
+odoo -c /etc/odoo/odoo.conf -i base --without-demo=all
+
+echo "➡ Starting Odoo server..."
 exec odoo -c /etc/odoo/odoo.conf
